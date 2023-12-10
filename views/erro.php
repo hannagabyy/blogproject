6a058cb3061f3e349cf60e8d2249e45a807754cf
hannagabyy <hannarocha000@gmail.com>
2023-12-10 14:07:00 -0300

    Adicionando página de erro

---
+++ views/erro.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/style.css">
+    <title>Página Não Encontrada</title>
+</head>
+<body>
+    <div class="not-found">
+        <img class="not-found__imagem" src="../images/not-found.svg" alt="Imagem de página não encontrada">
+    </div>
+</body>
+</html>
