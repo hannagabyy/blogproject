97aba0f010b915f51b2aa81b07c408ab0bc3b92b
LFelipeSN <lufnepomuceno@gmail.com>
2023-12-30 00:43:22 -0300

    Adicionando funcionalidade de cadastrar post

---
+++ controller/cadastrar_post.controller.php
@@ -0,0 +1,25 @@
+<?php
+include(__DIR__."/../includes.php");
+include(__DIR__."/../protect.php");
+require_once("../../Daofactory/usuarios.php");
+require_once("../../Daofactory/post.php");
+
+
+//Criação do post:
+if(isset($_POST['editor'])) {
+
+    $tagsPermitidas = '<p><ul><li><i><ol><h1><h2><h3><h4><strong>';
+    $comentario = strip_tags($_POST['editor'], $tagsPermitidas);
+    $comentario =  htmlspecialchars($comentario);
+
+    // $comentario =  $_POST['editor'];
+    $usuarioId = filter_var($_SESSION['id'], FILTER_VALIDATE_INT);
+
+    Post::insertPost($comentario, $usuarioId);
+
+   
+} else{
+    //alerta de erro
+}
+
+header("Location: ../home.php");
