69ec858de60698b038c5da2c926a28e75b9f400b
LFelipeSN <lufnepomuceno@gmail.com>
2024-01-27 22:22:22 -0300

    retirando consoles obsoletos

--- javascript/busca_amigos.js
+++ javascript/busca_amigos.js
@@ -14,23 +14,19 @@ function buscarAmigo(nomePesquisa){
     xhttp.onload = function() {
         caixaDeResultado.style.display='block';
         caixaDeResultado.innerHTML = this.responseText;
-        console.log(this.responseText);
         document.querySelectorAll('.card-amigo-resultado').forEach((card) => {
             card.querySelector('.icone-adicionar').addEventListener('click', () => {  
                 let icone = card.querySelector('.icone-adicionar');
                 let linkPerfil = card.querySelector('.card-nome').parentElement.getAttribute('href');               
                 let url = new URL(linkPerfil, window.location.href);
 
-                console.log(url.searchParams.get("id"));
-
                 xhttp2.open("POST", "../../controller/ajax_controllers/adicionar_amigo.php");
                 xhttp2.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
                 xhttp2.send("amigoId="+url.searchParams.get("id")); 
-                
+
 
                 if(icone.src.endsWith('/add-friend-icon.svg')){
                     icone.src = icone.src.replace('add', 'added');
-                    console.log('teclou no bt');
 
                 }
                 //else if(caminhoIcone.endsWith('/add-friend-icon.svg')){
