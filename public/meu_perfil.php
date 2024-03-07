8951cceb4d73eca4b41d169c2f64f8154c1b0ecd
LFelipeSN <lufnepomuceno@gmail.com>
2024-03-06 23:27:15 -0300

    unindo controller de "perfil" com "meu_perfil"

--- public/meu_perfil.php
+++ public/meu_perfil.php
@@ -1,5 +1,3 @@
 <?php
-include(__DIR__.'/../controller/meu_perfil.controller.php');
+include(__DIR__.'/../controller/perfil.controller.php');
 include(__DIR__.'/../views/meu_perfil.view.php');
-
-echo "<script>toastMessagem('fa-square-check','Sua alterações foram salvas com sucesso!', '#63E6BE') </script>";
