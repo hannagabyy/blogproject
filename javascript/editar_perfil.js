b543eb6c71b901ac17e32138a0f9a98f37c74d2a
LFelipeSN <lufnepomuceno@gmail.com>
2024-03-14 00:22:53 -0300

    melhorias ao editar perfil

---
+++ javascript/editar_perfil.js
@@ -0,0 +1,37 @@
+let salvarPerfil = document.getElementById("salvarPerfil");
+let fotoUsuario = document.getElementById("fotoUsuario");
+let inputImage = document.getElementById("inputImage");
+
+fotoUsuario.addEventListener('click',function (e){
+    inputImage.click();    
+});
+
+inputImage.addEventListener('change',function (e){
+    let image = document.createElement('img');
+    let imageSrc = URL.createObjectURL(inputImage.files[0]);
+    fotoUsuario.src = imageSrc;
+});
+
+salvarPerfil.addEventListener('submit',function (e){
+    e.preventDefault();
+    const xhttp = new XMLHttpRequest();
+    let formData = new FormData(salvarPerfil);
+
+    xhttp.onload = function() {
+        let resultado = this.responseText;
+        
+        if(resultado == true){
+            toastMessagem('fa-square-check', 'Sua alterações foram salvas com sucesso!', '#63E6BE');
+            // setTimeout(function () {
+            //     location.reload();
+            // }, 2500);          
+        }else{
+            toastMessagem('fa-triangle-exclamation','Algo deu errado, tente novamente!','red');
+        }
+    }
+
+    xhttp.open("POST", "../controller/ajax_controllers/atualiza_perfil.php");
+    // xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send(formData);
+    
+});
--- views/meu_perfil.view.php
+++ views/meu_perfil.view.php
@@ -2,20 +2,20 @@
 
 <h1 class="titulo-principal text-center display-2 mt-5 text-light">Edite seu perfil</h1>
 <main class="col-md-10 mx-auto">
-    <form action="" method="post" enctype="multipart/form-data" class="d-flex flex-column justify-content-center align-items-center my-4 w-100 container-perfil">
+    <form id="salvarPerfil" method="post" enctype="multipart/form-data" class="d-flex flex-column justify-content-center align-items-center my-4 w-100 container-perfil">
         <img id="fotoUsuario" class="fotoUsuario-perfil bg-white rounded-circle" src="../<?= $usuario['foto'] ?>">
         <label class="editar-foto-perfil m-2" for="fotoPerfil">
             Escolha sua foto de perfil
         </label>
 
-        <input type="file" name="fotoPerfil" class="editar-foto-perfil" accept="image/*" />
+        <input id="inputImage" type="file" name="fotoPerfil" class="editar-foto-perfil" accept="image/*" hidden />
 
         <div class="div-editar-meu-perfil">
             <input type="hidden" name="id" value="<?= $usuario['id'] ?>">
             
             <div class="input-form__perfil">
-                <label for="email" class="form-label text-light">Nome</label>
-                <input type="email" class="form-control input-editar" name="email" value="">
+                <label for="nome" class="form-label text-light">Nome</label>
+                <input type="text" class="form-control input-editar" name="nome" value="<?= $usuario['nome']?>">
             </div>
 
             <div class="input-form__perfil">
@@ -28,13 +28,13 @@
             </div>
 
             <div class="input-form__perfil">
-                <label for="senha" class="form-label text-light">Senha</label>
-                <input type="password" class="form-control input-editar" name="senha" value="">
+                <label for="senha" class="form-label text-light">Nova Senha</label>
+                <input type="password" class="form-control input-editar" name="senha" autocomplete="new-password">
             </div>
 
             <div class="input-form__perfil">
                 <label for="confirmarSenha" class="form-label text-light">Confirmar Senha</label>
-                <input type="password" class="form-control input-editar" name="confirmarSenha" value="">
+                <input type="password" class="form-control input-editar" name="confirmarSenha">
             </div>
 
             <div class="input-form__perfil">
