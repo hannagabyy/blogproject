465192641ffc0ab9a8ce33d57faf9791153b29c7
LFelipeSN <lufnepomuceno@gmail.com>
2024-05-04 02:19:23 -0300

    respostas ao adicionar ou excluir amigo

--- controller/ajax_controllers/atualiza_amizade.php
+++ controller/ajax_controllers/atualiza_amizade.php
@@ -3,19 +3,29 @@ require_once(__DIR__."/../../includes.php");
 require_once(__DIR__."/../../protect.php");
 require_once("../../Daofactory/amizade.php");
 
-$amigo_id = filter_input(INPUT_POST, 'amigoId', FILTER_VALIDATE_INT);
-$usuario_id = filter_var($_SESSION['id'], FILTER_VALIDATE_INT);
+$amigo_id = filter_input(INPUT_POST, 'amigoId', FILTER_SANITIZE_NUMBER_INT);
+$usuario_id = filter_var($_SESSION['id'], FILTER_SANITIZE_NUMBER_INT);
+
+/* atualização */ 
+$resultado = [
+    'sucesso'=> false,
+    'msg' => 'Algo deu errado, tente novamente!'
+];
+
 
 if(isset($amigo_id) && !empty($amigo_id)){
     $sao_amigos = Amizade::verificaAmizade($usuario_id, $amigo_id);
 
     /* cria nova amizade */
     if(!$sao_amigos){
-        Amizade::insertAmizade($usuario_id, $amigo_id);
+        $resultado['sucesso'] = Amizade::insertAmizade($usuario_id, $amigo_id);
+        $resultado['msg'] = "Amigo Adicionado!";
 
     /* desfaz a amizade */
-    }else{
-        Amizade::deleteAmizade($usuario_id, $amigo_id);
-
+    }
+    else{
+        $resultado['sucesso'] = Amizade::deleteAmizade($usuario_id, $amigo_id);
+        $resultado['msg'] = "Amigo removido!";
     }
 }
+print(json_encode($resultado));
